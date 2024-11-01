909a49ea34659d94ddb564410d67d4d559ca4ea7
andres_cuevas <andres-felipe32@hotmail.com>
2024-11-01 03:39:24 -0500

    imagenes de los resources

--- Frontend/ikbo/src/routes/MainRoutes.jsx
+++ Frontend/ikbo/src/routes/MainRoutes.jsx
@@ -9,13 +9,13 @@ import { Navigate } from 'react-router';
 import CreateProductForm from 'pages/dashboard/products/CreateProductForm';
 import ListInventaries from 'pages/dashboard/inventary/ListInventaries';
 
-const Color = Loadable(lazy(() => import('pages/component-overview/color')));
-const Typography = Loadable(lazy(() => import('pages/component-overview/typography')));
-const Shadow = Loadable(lazy(() => import('pages/component-overview/shadows')));
-const DashboardDefault = Loadable(lazy(() => import('pages/dashboard/index')));
+// const Color = Loadable(lazy(() => import('pages/component-overview/color')));
+// const Typography = Loadable(lazy(() => import('pages/component-overview/typography')));
+// const Shadow = Loadable(lazy(() => import('pages/component-overview/shadows')));
+// const DashboardDefault = Loadable(lazy(() => import('pages/dashboard/index')));
 
-// render - sample page
-const SamplePage = Loadable(lazy(() => import('pages/extra-pages/sample-page')));
+// // render - sample page
+// const SamplePage = Loadable(lazy(() => import('pages/extra-pages/sample-page')));
 
 // ==============================|| MAIN ROUTING ||============================== //
 
--- README.md
+++ README.md
@@ -35,3 +35,18 @@ en el fichero `docker-compose.yml` se encuentran las variables de entorno de cad
 | Backend Product Services | 3001  |
 | BBDD Postgres | 5433 |
 
+
+### Resultados esperados
+
+Si todo ha ido bien en la url `http://localhost` estara disponible el frontend. Y desde alli se podran realizar las pruebas de los servicios basicos implementados.
+
+- Registrar usuario
+- Iniciar Sesion
+- Crear Producto
+- Listar productos
+- Agregar/Descontar inventario
+- Listar Inventario
+
+![Diseño del frontend](Resources/login_registered.png)
+
+![Diseño del frontend productos](Resources/products_inventory.png)
